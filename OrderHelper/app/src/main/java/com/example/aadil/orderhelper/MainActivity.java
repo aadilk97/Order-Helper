d0ebb06b8af992f05e9785dbd03cef6f93d2443c
Aadil <33660637+aadilk97@users.noreply.github.com>
2018-02-12 20:39:16 +0530

    Update MainActivity.java

--- OrderHelper/app/src/main/java/com/example/aadil/orderhelper/MainActivity.java
+++ OrderHelper/app/src/main/java/com/example/aadil/orderhelper/MainActivity.java
@@ -2,11 +2,9 @@ package com.example.aadil.orderhelper;
 
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
+import java.util.*;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.os.AsyncTask;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,10 +20,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText msg, ip;
     private Button btn;
     private ListView lst;
-    private static int c = -1;
-    private static Socket s[] = new Socket[20];
+    private static Vector v = new Vector();
     private ArrayList<String> orders = new ArrayList<String>();
     ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +33,11 @@ public class MainActivity extends AppCompatActivity {
         ip = (EditText) findViewById(R.id.editText2);
         btn = (Button) findViewById(R.id.button);
         lst = (ListView) findViewById(R.id.listView);
-
-        //orders.add("test");
-
+        
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                c++;
                 Writer obj = new Writer(msg.getText().toString(), ip.getText().toString());
                 obj.start();
             }
@@ -71,13 +66,14 @@ public class MainActivity extends AppCompatActivity {
         }
         public void run(){
             try{
-                s[++c] = new Socket(ip, 8080);
-                DataOutputStream dout = new DataOutputStream(s[c].getOutputStream());
+                Socket s = new Socket(ip, 8080);
+                v.addElement(s);
+                DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 
                 dout.writeUTF(message);
                 dout.flush();
 
-                Reader r = new Reader(s[c], c);
+                Reader r = new Reader(s);
                 r.start();
 
             }
@@ -89,18 +85,17 @@ public class MainActivity extends AppCompatActivity {
 
     class Reader extends Thread{
         Socket s;
-        int id;
-        Reader(Socket s, int id){
+        Reader(Socket s){
             this.s = s;
-            this.id = id;
         }
         public void run(){
-            while (true) {
+            boolean flag = true;
+            while (flag) {
                 try {
                     DataInputStream din = new DataInputStream(s.getInputStream());
                     final String server_msg = din.readUTF();
 
-                    System.out.println("Server says " + server_msg + "for " + id);
+                    System.out.println("Server says " + server_msg);
 
                     runOnUiThread(new Runnable() {
                         @Override
@@ -112,8 +107,9 @@ public class MainActivity extends AppCompatActivity {
 
                         }
                     });
-//                    s.close();
-//                    c--;
+                    v.remove(s);
+                    s.close();
+                    flag = false;
 
                 } catch (Exception e) {
                     e.printStackTrace();
